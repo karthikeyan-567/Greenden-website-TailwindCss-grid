c1fd148727cba5d8d0ae1da9e4c53af9a03b7528
karthikeyan-567 <karthikeyan672005@gmail.com>
2025-01-31 18:11:36 +0530

    greenden is uploaded sus

---
+++ new.js
@@ -0,0 +1,14 @@
+// 
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const users = [
+//     { name: "Alice"},
+ 
+//     { name: "Bob"}
+//   ]
+  
+//   const names = users.map(use => use.name);
+//   console.log(names); 
+const numbers = [1, 2, 3, 4, 5];
+
+let  sum = numbers.reduce((acc , num) => acc + num,0);
+console.log(sum); 
